83d1270d1eece1729ae432ec8db113c33e2bedf4
SairaAlam <sairanalam@gmail.com>
2020-02-23 16:49:05 -0500

    Update FunctionsWI.py

--- WI_Functions/FunctionsWI.py
+++ WI_Functions/FunctionsWI.py
@@ -1,37 +1,68 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 29 08:01:37 2020
+Created on Sun Feb 23 14:05:12 2020
 
 @author: Saira
-"""        
-# Some notes on how we might organize the functions
+"""
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 19:40:45 2020
+
+@author: Saira
+"""
+
 import pandas as pd 
- # excel with lunch data
-df = pd.read_excel("C:\\Users\\Saira\\Desktop\\DAEN 690\\ShareOurStrength-master\\Data\\Wisconsin_WI\\Raw_Data\\2017_2018_NSLP_MEAL_PARTICIPATION.xls")
-df.rename(columns={"AGENCY_CODE": "District ID", "AGENCY_NAME": "District Name", "DPI_SITE_CODE": "School ID", "SCHOOL_NAME": "School Name", "DATE_CLAIM": "Claim Date", "CEP_PARTICIPATION": "CEP (Y/N)", "ENROLLMENT": "Enrollment-Total", "DAYS_OPERATING": "Operating-Days-Breakfast Only", "APPROVED_FREE": "Enrollment-Free", "APPROVED_REDUCED": "Enrollment-Reduced", "FOOD_FREE": "Breakfast Meals-Free", "FOOD_REDUCED_PRICE": "Breakfast Meals-Paid"}, inplace = True)
-df.drop('PROGRAM', 1, inplace=True) 
-df['Claim Date'] = df['Claim Date'].str.replace('01-', '')
-#df['Claim Date']= pd.to_datetime(df['Claim Date'])
-#for col in df.columns:
-#    print(col)
-#print(df['Claim Date'])
-#df.info()
 
-#excel with breakfast data
-df2 = pd.read_excel("C:\\Users\\Saira\\Desktop\\DAEN 690\\ShareOurStrength-master\\Data\\Wisconsin_WI\\Raw_Data\\2017_2018_SBP_MEAL_PARTICIPATION.xls")
-df2.rename(columns={"AGENCY_CODE": "District ID", "AGENCY_NAME": "District Name", "DPI_SITE_CODE": "School ID", "SCHOOL_NAME": "School Name", "DATE_CLAIM": "Claim Date", "CEP_PARTICIPATION": "CEP (Y/N)", "ENROLLMENT": "Enrollment-Total", "DAYS_OPERATING": "Operating-Days-Breakfast Only", "APPROVED_FREE": "Enrollment-Free", "APPROVED_REDUCED": "Enrollment-Reduced", "FOOD_FREE": "Breakfast Meals-Free", "FOOD_REDUCED_PRICE": "Breakfast Meals-Paid"}, inplace = True)
-df2['TRADITIONAL_MODEL'] = df2['TRADITIONAL_MODEL'].str.replace('Y', 'TRADITIONAL_MODEL')
-df2['MID_MORNING_MODEL'] = df2['MID_MORNING_MODEL'].str.replace('Y', 'MID_MORNING_MODEL')
-df2['REDUCED_PRICE_MODEL'] = df2['REDUCED_PRICE_MODEL'].str.replace('Y', 'REDUCED_PRICE_MODEL')
-df2['GRAB_N_GO_MODEL'] = df2['GRAB_N_GO_MODEL'].str.replace('Y', 'GRAB_N_GO_MODEL')
-df2['FREE_MODEL'] = df2['FREE_MODEL'].str.replace('Y', 'FREE_MODEL')
-df2['Breakfast Delivery Model from State Agency Tracking-Original'] = df2['TRADITIONAL_MODEL'] + "," + df2['MID_MORNING_MODEL'] + "," + df2['CLASSROOM_MODEL'] + "," + df2['REDUCED_PRICE_MODEL'] + "," + df2['GRAB_N_GO_MODEL'] + "," + df2['FREE_MODEL']
-df2['Breakfast Delivery Model from State Agency Tracking-Original'] = df2['Breakfast Delivery Model from State Agency Tracking-Original'].str.replace('N', '')
-#print(df2['Breakfast Delivery Model from State Agency Tracking-Original'])
-df2['School ID'] = df2['School ID'].apply('{:0>4}'.format)
-df2['District ID'] = df2['District ID'].apply('{:0>6}'.format)
-
-print(df2['District ID'])
-#for col in df2.columns:
-#    print(col)
+def rename(filename, raw_field_name, clean_field_name):
+    excel_dataframe = pd.read_excel(filename, header = 0)
+#    print(excel_dataframe.head())
+    excel_dataframe.rename(columns={getattr(row,"RAW_FIELD_NAME") : getattr(row,"TRANSFORMATION")}, inplace=True)
+    print(excel_dataframe.head())
+    excel_dataframe.to_excel(filename)
+    excel_dataframe.save()
     
+def changetype(filename, raw_field_name, clean_field_name):
+    excel_dataframe = pd.read_excel(filename, header = 0)
+    excel_dataframe[raw_field_name] = excel_dataframe[raw_field_name].astype('str')
+    print(excel_dataframe.dtypes[raw_field_name])
+
+def changedate(filename, raw_field_name):
+    excel_dataframe = pd.read_excel(filename, header = 0)
+    excel_dataframe[raw_field_name] = pd.to_datetime(df[raw_field_name].dt.strftime('%M/%D/%Y'))
+    print(excel_dataframe.dtypes[raw_field_name])
+    
+def prefix_zeroes(filename, raw_field_name, clean_field_name):
+    print(clean_field_name)
+    excel_dataframe = pd.read_excel(filename, header = 0)
+    excel_dataframe[raw_field_name] = excel_dataframe[raw_field_name].str.zfill(clean_field_name)
+    print(excel_dataframe[raw_field_name])
+#    df2['School ID'] = df2['School ID'].apply('{:0>4}'.format)
+
+
+        
+    
+import sys
+
+
+state = sys.argv[1]
+ # excel with lunch data
+if state == "WI":
+    df = pd.read_excel(r"C:\Users\Saira\Desktop\DAEN690\ShareOurStrength-master\Data\Wisconsin_WI\Raw_Data\TestData.xlsx", header = 0)
+else: 
+    print("script does not exist for this state")
+
+for row in df.itertuples(index=True,name='Pandas'):
+    filename = getattr(row,"RAW_DATA_FILE")
+    raw_field_name = getattr(row, "RAW_FIELD_NAME")
+    clean_field_name = getattr(row,"TRANSFORMATION")
+    if getattr(row,"OPERATION") == "RENAME":
+        rename(filename, raw_field_name, clean_field_name)
+#    elif getattr(row,"OPERATION") == "CHANGETYPE":
+#        changetype(filename, raw_field_name, clean_field_name)
+#    elif getattr(row,"OPERATION") == "CHANGEDATE":
+#        changedate(filename, raw_field_name)
+#    elif getattr(row,"OPERATION") == "PREFIX_ZEROES":
+#        prefix_zeroes(filename, raw_field_name, clean_field_name)
+
+
+
